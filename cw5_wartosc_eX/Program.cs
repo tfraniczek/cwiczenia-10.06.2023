f220c6790e4199c6d31d6629d2e5ea058e069c84
tfraniczek <tymoteusz.franiczek@gmail.com>
2023-06-12 21:29:09 +0200

    1

--- cw5_wartosc_eX/Program.cs
+++ cw5_wartosc_eX/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace cw5_wartosc_eX
+public class MyProgram
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int numerPrzyblizenia, i, n;
+        double wartoscPrzyblizenia;
+
+        Console.WriteLine("Podaj wielkosc przyblizenia");
+        numerPrzyblizenia = (int)inputValue();
+        for (i = 1; i <= numerPrzyblizenia; i++)
         {
+            wartoscPrzyblizenia = Math.Pow(numerPrzyblizenia, i) / (numerPrzyblizenia * i);
+            wartoscPrzyblizenia = wartoscPrzyblizenia + Math.Pow(numerPrzyblizenia, i) / (numerPrzyblizenia * i);
         }
+        Console.WriteLine("Wynik obliczen to " + wartoscPrzyblizenia);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
     }
 }
