9e494d0b6d72a9c7289df5e015a591d8e3d52fc1
tfraniczek <tymoteusz.franiczek@gmail.com>
2023-06-12 18:24:34 +0200

    Create Program.cs

---
+++ cw9_odwracanie_liczb/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw9_odwracanie_liczb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        string liczba;
+            Console.WriteLine("Wpisz liczbę, którą chcesz odwrócić:");
+            liczba = Console.ReadLine();
+            char[] stringArray = liczba.ToCharArray();
+            Array.Reverse(stringArray);
+            string odwrocona_tablica = new string(stringArray);
+            Console.WriteLine("Liczba po odwróceniu to: " + odwrocona_tablica);
+            Console.ReadLine();
+
+
+        }
+    }
+}
